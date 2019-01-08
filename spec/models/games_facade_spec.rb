c96133f49753d0393c4e6a12db39675c5faff245
andrewetobin <andrewetobin@gmail.com>
2019-01-08 11:31:40 -0700

    Adds tests for GamesFacade

--- spec/models/games_facade_spec.rb
+++ spec/models/games_facade_spec.rb
@@ -16,8 +16,14 @@ describe GamesFacade, type: :model do
         expect(games_facade.results[0]).to have_key(:DateTime)
         expect(games_facade.results[0]).to have_key(:AwayTeam)
         expect(games_facade.results[0]).to have_key(:HomeTeam)
-
       end
+    end
+    it "give right team name" do
+      games_facade = GamesFacade.new
+
+      expect(games_facade.teams["BOS"]).to eq("Boston Bruins")
+      expect(games_facade.teams["COL"]).to eq("Colorado Avalanche")
+      expect(games_facade.teams["STL"]).to eq("St. Louis Blues")
     end
   end
 end
