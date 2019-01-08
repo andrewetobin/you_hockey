584854775ed4d331a2d75328628d3b89e3baefa3
andrewetobin <andrewetobin@gmail.com>
2019-01-07 19:50:02 -0700

    Adds teams hash to reference on games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,8 @@ class GamesController < ApplicationController
 
   def index
     @games_facade = GamesFacade.new
+    @games = @games_facade.results
+    @teams = @games_facade.teams
   end
 
 end
--- app/models/games_facade.rb
+++ app/models/games_facade.rb
@@ -1,5 +1,39 @@
 class GamesFacade
 
+  def teams
+    { "STL" => "St. Louis Blues",
+    "PHI" => "Philadelphia Flyers",
+    "NAS" => "Nashville Predators",
+    "TOR" => "Toronto Maple Leafs",
+    "MIN" => "Minnesota Wild",
+    "MON" => "Montreal Canadiens",
+    "CGY" => "Calgary Flames",
+    "CHI" => "Chicago Blackhawks",
+    "LA" => "Los Angeles Kings",
+    "SJ" => "San Jose Sharks",
+    "BOS" => "Boston Bruins",
+    "NJ" => "New Jersey Devils",
+    "BUF" => "Buffalo Sabres",
+    "CAR" => "Carolina Hurricanes",
+    "NYI" => "New York Islanders",
+    "FLA" => "Florida Panthers",
+    "PIT" => "Pittsburg Penguins",
+    "DET" => "Detriot Red Wings",
+    "CBJ" => "Columbus Blue Jackets",
+    "TB" => "Tampa Bay Lightning",
+    "DAL" => "Dallas Stars",
+    "COL" => "Colorado Avalanche",
+    "WPG" => "Winnepeg Jets",
+    "NYR" => "New York Rangers",
+    "VEG" => "Vegas Golden Knights",
+    "EDM" => "Edmonton Oilers",
+    "OTT" => "Ottawa Senators",
+    "ANA" => "Anaheim Mighty Ducks",
+    "WAS" => "Washington Capitals",
+    "ARI" => "Arizona Coyotes",
+    "VAN" => "Vancouver Canucks"
+   }
+  end
 
   def results
     service.games
